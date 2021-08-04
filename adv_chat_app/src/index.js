fd204a52b93a162c965af4cbe244e2c797dc7b15
Shadman Chowdhury <chowdhuryshadman0@gmail.com>
2021-08-04 14:38:32 -0400

    Init

---
+++ adv_chat_app/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+
+const Main = () => {
+  return (
+    <>
+      <h1>It works!</h1>
+    </>
+  );
+}
+
+ReactDOM.render(<Main/>,document.getElementById('root'));
